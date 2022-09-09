48fcbf9a65bb048d7e5d0763d774c2f5606430fb
Yander Alves de Carvalho <yander.carvalho@fitbank.com.br>
2022-09-09 14:48:36 -0300

    Adicao do Desafio do estacionamento

--- ProjetoDIOEstacionamento/obj/Debug/net5.0/ProjetoDIOEstacionamento.AssemblyInfo.cs
+++ ProjetoDIOEstacionamento/obj/Debug/net5.0/ProjetoDIOEstacionamento.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DesafioFundamentos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoDIOEstacionamento")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("DesafioFundamentos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DesafioFundamentos")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProjetoDIOEstacionamento")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProjetoDIOEstacionamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
