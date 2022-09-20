220481535bd9a2b89962bdc7f470a0dba032632f
Yander Alves de Carvalho <yander.carvalho@fitbank.com.br>
2022-09-20 14:16:45 -0300

    Adição do desafio do Smartphone

---
+++ ProjetoSmartphone/Iphone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSmartphone
+{
+    public class Iphone : Smartphone
+    {
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando {nome} no Aparelho Iphone");
+        }
+    }
+}
---
+++ ProjetoSmartphone/Nokia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSmartphone
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando {nome} no aparelho Nokia");
+        }
+    }
+}
