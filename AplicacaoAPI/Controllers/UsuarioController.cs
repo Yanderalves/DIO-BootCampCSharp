1d76017658da413bc25a61d5de1b08f08c64f003
Yander Alves de Carvalho <yander.carvalho@fitbank.com.br>
2022-10-27 15:09:01 -0300

    Adição do projeto de API

---
+++ AplicacaoAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicacaoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataEHora")]
+        public IActionResult ObterdataEHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var msg = $"Olá, {nome}, seja bem vindo";
+
+            return Ok(new { msg });
+        }
+    }
+}
+ 
