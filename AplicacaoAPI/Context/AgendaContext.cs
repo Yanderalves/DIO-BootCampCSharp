b54317a895ae9666d77d014d623b543ec676c263
Yander Alves <yanderceara@gmail.com>
2022-11-06 23:05:05 -0300

    Classe entidade, contexto e conexão

---
+++ AplicacaoAPI/Context/AgendaContext.cs
@@ -0,0 +1,13 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options){
+
+        }
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
--- AplicacaoAPI/Program.cs
+++ AplicacaoAPI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace AplicacaoAPI
 {
@@ -16,11 +19,22 @@ namespace AplicacaoAPI
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+    public class BloggingContextFactory : IDesignTimeDbContextFactory<AgendaContext>
+    {
+        public AgendaContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AgendaContext>();
+            optionsBuilder.UseSqlServer(connectionString:"server=localhost\\sqlexpress;Initial Catalog=Agenda; Integrated Security=true");
+
+            return new AgendaContext(optionsBuilder.Options);
+        }
     }
 }
