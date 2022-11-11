b7405b2b0546bb49c84cc449f65809f91b90a589
Yander Alves de Carvalho <yander.carvalho@fitbank.com.br>
2022-11-11 15:49:04 -0300

    Confg. do context, criação da Pag Cadastro contato

--- AplicacaoMVC/Context/ContatoContext.cs
+++ AplicacaoMVC/Context/ContatoContext.cs
@@ -2,12 +2,18 @@
 // using System.Collections.Generic;
 // using System.Linq;
 // using System.Threading.Tasks;
+// using AplicacaoMVC.Models;
 // using Microsoft.EntityFrameworkCore;
 
 // namespace AplicacaoMVC.Context
 // {
 //     public class ContatoContext : DbContext
 //     {
-//         public ContatoContext(DbContext)
+//         public ContatoContext(DbContextOptions<AgendaContext> options) : base(options)
+//         {
+
+//         }
+
+//         public DbSet<Contato> Contatos { get; set; }
 //     }
 // }
